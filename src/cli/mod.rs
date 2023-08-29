4a9e41e8c3345471875b65e69d31ba95d6da64e8
Luc Perkins <lucperkins@gmail.com>
2023-08-29 20:40:13 +0100

    Hide host flag from help output

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,8 +1,9 @@
 pub(crate) mod cmd;
 
+/// fh: a CLI for interacting with FlakeHub, a platform for discovering and publishing Nix flakes from Determinate Systems.
 #[derive(clap::Parser)]
 pub(crate) struct Cli {
-    #[clap(long, default_value = "https://api.flakehub.com")]
+    #[clap(long, default_value = "https://flakehub.com", hide = true)]
     pub host: String,
 
     #[clap(subcommand)]
