2da6b495394a0bad3dbcae8f8756f18b61113bf4
Cole Helbling <cole.helbling@determinate.systems>
2023-08-29 09:45:58 -0700

    add test flakes

---
+++ flake1.test.nix
@@ -0,0 +1,51 @@
+{
+  description = "cole-h's NixOS configuration";
+
+  inputs = {
+    # Flakes
+    # nixpkgs.url = "git+file:///home/vin/workspace/vcs/nixpkgs/master";
+    # nixpkgs.url = "git+file:///home/vin/workspace/vcs/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:nixos/nixpkgs/2f47650c2f28d87f86ab807b8a339c684d91ec56";
+    # nixpkgs.url = "github:nixos/nixpkgs/master";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-20.09";
+    # nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    agenix-cli = {
+      url = "github:cole-h/agenix-cli";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.darwin.follows = "darwin";
+      inputs.home-manager.follows = "home";
+    };
+    home = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix = {
+      url = "github:nixos/nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # nix = { url = "github:edolstra/nix/lazy-trees"; inputs.nixpkgs.follows = "nixpkgs"; };
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Not flakes
+    wezterm = {
+      url = "git+https://github.com/wez/wezterm.git?submodules=1";
+      flake = false;
+    };
+  };
+
+  outputs = inputs: {};
+}
