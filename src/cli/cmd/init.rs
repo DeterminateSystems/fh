f1d2a2a5e9f32fd715deb68cddcf0df53dca4abf
Luc Perkins <lucperkins@gmail.com>
2023-09-01 18:45:23 +0100

    s/Rust/Go

--- src/cli/cmd/init.rs
+++ src/cli/cmd/init.rs
@@ -117,7 +117,7 @@ impl CommandExecute for InitSubcommand {
         }
 
         // Go projects
-        if project.is_go_project() && Prompt::bool("This seems to be a Go project. Would you like to initialize your flake with built-in Rust dependencies?")?{
+        if project.is_go_project() && Prompt::bool("This seems to be a Go project. Would you like to initialize your flake with built-in Go dependencies?")?{
             if inputs.get("nixpkgs").is_none() {
                 if Prompt::bool(
                     "You'll need a Nixpkgs input for Go projects. Would you like to add one?",
