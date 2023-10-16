5aaf93b6f7a09cb642be1c5946a5f970a9a795de
Luc Perkins <lucperkins@gmail.com>
2023-10-16 14:09:35 +0300

    Use FlakeHub URLs for flake-compat tarballs

--- assets/shell.nix
+++ assets/shell.nix
@@ -2,7 +2,7 @@
   (
     let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
     fetchTarball {
-      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      url = lock.nodes.flake-compat.locked.url;
       sha256 = lock.nodes.flake-compat.locked.narHash;
     }
   )
