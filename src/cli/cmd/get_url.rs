e6fcfc2d2911efd1827fe0f473180f67913c704d
Ryan Bernstein <ryanbernstein1@gmail.com>
2023-11-16 15:51:55 -0500

    simplify get-url interface

--- src/cli/cmd/get_url.rs
+++ src/cli/cmd/get_url.rs
@@ -4,18 +4,12 @@ use clap::Parser;
 
 use super::CommandExecute;
 
-/// Prints the URL of the given flake
+/// Prints the URL of a given FlakeHub flake
 #[derive(Parser, Debug)]
 pub(crate) struct GetURLSubcommand {
-    /// The name of the flake input.
+    /// The FlakeHub reference to print as a URL
     ///
-    /// If not provided, it will be inferred from the provided input URL (if possible).
-    #[clap(long)]
-    pub(crate) input_name: Option<String>,
-    /// The flake reference to add as an input.
-    ///
-    /// A reference in the form of `NixOS/nixpkgs` or `NixOS/nixpkgs/0.2305.*` (without a URL
-    /// scheme) will be inferred as a FlakeHub input.
+    /// A FlakeHub reference is of a form like `NixOS/nixpkgs` or `NixOS/nixpkgs/0.2305.*`
     pub(crate) input_ref: String,
 
     #[clap(from_global)]
@@ -26,7 +20,7 @@ pub(crate) struct GetURLSubcommand {
 impl CommandExecute for GetURLSubcommand {
     async fn execute(self) -> color_eyre::Result<ExitCode> {
         let (_, flake_input_url) =
-            crate::cli::cmd::add::infer_flake_input_name_url(self.api_addr, self.input_ref, self.input_name).await?;
+            crate::cli::cmd::add::infer_flake_input_name_url(self.api_addr, self.input_ref, None).await?;
 
         println!("{}", flake_input_url);
 
