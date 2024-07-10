77eef2afdc61efa4f15c567da7bebd3d827f154e
Luc Perkins <lucperkins@gmail.com>
2024-07-10 06:49:37 -0700

    Change wording of JSON option

--- src/cli/cmd/resolve.rs
+++ src/cli/cmd/resolve.rs
@@ -12,8 +12,8 @@ pub(crate) struct ResolveSubcommand {
     /// References must be of this form: {org}/{flake}/{version_req}#{attr_path}
     flake_ref: String,
 
-    /// Display the result as JSON displaying the store path plus the original attribute path.
-    #[arg(short, long)]
+    /// Output the result as JSON displaying the store path plus the original attribute path.
+    #[arg(long)]
     json: bool,
 
     #[clap(from_global)]
@@ -22,7 +22,7 @@ pub(crate) struct ResolveSubcommand {
 
 #[derive(Deserialize, Serialize)]
 pub(crate) struct ResolvedPath {
-    // The original attribute path, e.g. attr_path in {org}/{flake}/{version}#{attr_path}
+    // The original attribute path, i.e. attr_path in {org}/{flake}/{version}#{attr_path}
     attribute_path: String,
     // The resolved store path
     store_path: String,
