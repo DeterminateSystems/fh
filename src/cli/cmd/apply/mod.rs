b094865ce8f56e1de4283992a55d65a05d6129f1
Luc Perkins <lucperkins@gmail.com>
2024-07-25 11:15:36 -0700

    Make apply commands available only on specific systems

--- src/cli/cmd/apply/mod.rs
+++ src/cli/cmd/apply/mod.rs
@@ -1,5 +1,7 @@
 mod home_manager;
+#[cfg(target_os = "macos")]
 mod nix_darwin;
+#[cfg(target_os = "linux")]
 mod nixos;
 
 use std::{
@@ -22,8 +24,10 @@ use crate::{
 use self::{
     home_manager::{HomeManager, HOME_MANAGER_SCRIPT},
     nix_darwin::{NixDarwin, NIX_DARWIN_ACTION, NIX_DARWIN_PROFILE, NIX_DARWIN_SCRIPT},
-    nixos::{NixOs, NIXOS_PROFILE, NIXOS_SCRIPT},
 };
+
+#[cfg(target_os = "linux")]
+use self::nixos::{NixOs, NIXOS_PROFILE, NIXOS_SCRIPT};
 
 use super::{CommandExecute, FlakeHubClient};
 
@@ -42,9 +46,11 @@ enum System {
     /// Resolve the store path for a Home Manager configuration and run its activation script
     HomeManager(HomeManager),
 
+    #[cfg(target_os = "macos")]
     /// Resolve the store path for a nix-darwin configuration and run its activation script
     NixDarwin(NixDarwin),
 
+    #[cfg(target_os = "linux")]
     /// Apply the resolved store path on a NixOS system
     #[clap(name = "nixos")]
     NixOs(NixOs),
@@ -54,8 +60,10 @@ enum System {
 impl CommandExecute for ApplySubcommand {
     async fn execute(self) -> color_eyre::Result<ExitCode> {
         let output_ref = match &self.system {
-            System::NixOs(nixos) => nixos.output_ref()?,
             System::HomeManager(home_manager) => home_manager.output_ref()?,
+            #[cfg(target_os = "linux")]
+            System::NixOs(nixos) => nixos.output_ref()?,
+            #[cfg(target_os = "macos")]
             System::NixDarwin(nix_darwin) => nix_darwin.output_ref()?,
         };
 
@@ -78,6 +86,7 @@ impl CommandExecute for ApplySubcommand {
 
                 run_script(script_path, None, HOME_MANAGER_SCRIPT).await?;
             }
+            #[cfg(target_os = "macos")]
             System::NixDarwin(_) => {
                 let profile_path =
                     apply_path_to_profile(NIX_DARWIN_PROFILE, &resolved_path.store_path).await?;
@@ -92,6 +101,7 @@ impl CommandExecute for ApplySubcommand {
                 )
                 .await?;
             }
+            #[cfg(target_os = "linux")]
             System::NixOs(NixOs { action, .. }) => {
                 let profile_path =
                     apply_path_to_profile(NIXOS_PROFILE, &resolved_path.store_path).await?;
